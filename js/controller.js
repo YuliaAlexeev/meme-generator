2f34f68d648f48c62893149757571b46057cf47e
Yulia Alexeev <july90a@gmail.com>
2020-10-22 19:50:20 +0300

    changes in files

--- js/controller.js
+++ js/controller.js
@@ -2,23 +2,151 @@
 var gElCanvas;
 var gCtx;
 
-console.log('started');
 function onInit(){
-     gElCanvas = document.querySelector('#meme-canvas');
-     gCtx = gElCanvas.getContext('2d');
-     console.log('canvas properties:', gCtx)
-     drawText(10, 10)
+    renderImages();
+    gElCanvas = document.querySelector('#meme-canvas');
+    gCtx = gElCanvas.getContext('2d');
+    console.log('canvas properties:', gCtx)
 }
 
-function onWrite(ev){
-    console.log(ev);
-}
-
-function drawText(){
-    gCtx.font = '30px Impact';
-    gCtx.strokeStyle = 'red'
-    gCtx.fillStyle = '#000';
+function drawImg() {
+    var img = new Image()
+    var memeIdx = getMeme().selectedImgId;
+    var currentMemeUrl = getImageById(memeIdx).url;
     
-    gCtx.textAlign = 'center';
-    gCtx.fillText('Hello world', gElCanvas.width/2, gElCanvas.height/2);
+    img.src = `./${currentMemeUrl}`;
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
+        renderText()
+    }
+}
+
+function renderText(){
+    var currentMeme = getMeme();
+    drawText(currentMeme.lines[0].txt, 50, 50)
+}
+
+function drawText(text, x, y){ 
+    var currentMeme = getMeme(); 
+    var selectedLineIdx = currentMeme.selectedLineIdx;
+    
+    gCtx.lineWidth = '2'
+    gCtx.font = `${currentMeme.lines[selectedLineIdx].size}px ${currentMeme.lines[selectedLineIdx].font}`;
+    gCtx.fillStyle = currentMeme.lines[selectedLineIdx].color;
+    gCtx.strokeStyle = currentMeme.lines[selectedLineIdx].strokeColor;
+    gCtx.textAlign = currentMeme.lines[selectedLineIdx].align;
+    gCtx.fillText(text, x, y);
+    gCtx.strokeText(text, x, y);
+}
+
+function onTextColor(){
+    var currentMeme = getMeme();
+    var selectedLineIdx = currentMeme.selectedLineIdx;
+
+    var txtColor = document.querySelector('input[name="textColor"]').value;
+    currentMeme.lines[selectedLineIdx].color = txtColor;
+    setTextColor(txtColor);
+    drawImg();
+}
+
+function onStrokeColor(){
+    var currentMeme = getMeme();
+    var selectedLineIdx = currentMeme.selectedLineIdx;
+
+    var strkColor = document.querySelector('input[name="strokeColor"]').value;
+    currentMeme.lines[selectedLineIdx].strokeColor = strkColor;
+
+    gStrokeColor = strkColor;
+}
+
+
+function onTextType(){
+    var text = document.querySelector('input[name=textType]').value;
+    setMemeText(text);
+    drawImg();
+}
+
+function onSelectedImg(imageId){
+    selectImg(imageId)
+    drawImg();
+}
+
+function onAddNewLine(){
+
+    var newText = 'New Line';
+    document.querySelector('input[name=textType]').value = newText;
+ 
+    drawText(newText, gElCanvas.width/2, gElCanvas.height/2);
+    setNewLine(newText);
+}
+
+function renderImages(){
+    var images = getImages();
+    var htmlStr = '';
+
+    images.forEach(img => {
+        htmlStr += `<img src="${img.url}" alt="" title="" onclick="onSelectedImg(${img.id})"/>`;
+    });
+    document.querySelector('.gallery').innerHTML = htmlStr;
+}
+
+function onTextAlign(align){
+    console.log('align', align);
+    if(align === 'right'){
+        gAlign = 'right';
+    } else if(align === 'left'){
+        gAlign = 'left';
+    }   else if(align === 'center'){
+        gAlign = 'center';
+    }
+    setTextAlign(gAlign);
+    drawImg();
+}
+
+
+
+function onFontFamily(fontStyle){
+    if(fontStyle === 'Arial'){
+        gFontStyle = 'Arial';
+    } else if(fontStyle === 'Times New Roman'){
+        gFontStyle = 'Times New Roman';
+    } else if(fontStyle === 'Cursive'){
+        gFontStyle = 'Cursive';
+    }   else if(fontStyle === 'Monospace'){
+        gFontStyle = 'Monospace';
+    }   else if(fontStyle === 'Tahoma'){
+        gFontStyle = 'Tahoma';
+    }
+    else{
+        gFontStyle = 'Impact';
+    }
+    setFontFamily(gFontStyle);
+    drawImg();
+}
+
+
+
+// function onFontSize(size){
+//     var i = 1;
+ 
+//     document.querySelector('.increase').innerText = i++;
+// }
+
+
+
+
+
+
+
+
+
+function clearCanvas() {
+    gCtx.clearRect(0, 0, gElCanvas.width, gElCanvas.height);
+}
+
+function downloadCanvas(elLink) {
+    const data = gElCanvas.toDataURL()
+    console.log('downloadCanvas',data);
+    elLink.href = data;
+    elLink.download = 'my-meme.jpg';
 }
--- js/service.js
+++ js/service.js
@@ -1,13 +1,76 @@
-var gKeywords = {'happy': 12,'funny puk': 1}
-var gImgs = [{id: 1, url: 'img/popo.jpg', keywords: ['happy']}];
+var gKeywords = {'happy': 12,'funny puk': 1};
 
 var gMeme = {
- selectedImgId: 5,
- selectedLineIdx: 0,
- lines: [{
-    txt: 'I never eat Falafel',
-    size: 20,
-    align: 'left',
-    color: 'red'
-}]
+    selectedImgId: 1,
+    selectedLineIdx: 0,
+    lines: [{
+        txt: 'I never eat Falafel',
+        size: 40,
+        align: 'left',
+        color: 'red',
+        strokeColor: 'black',
+        font: 'Impact',
+        x: 0,
+        y: 0
+    }]
 }
+
+var gImgs = [{id: 1, url: 'gallery/1.jpg', keywords: ['happy']},
+             {id: 2, url: 'gallery/2.jpg', keywords: ['happy']},
+             {id: 3, url: 'gallery/3.jpg', keywords: ['happy']},
+             {id: 4, url: 'gallery/4.jpg', keywords: ['happy']},
+             {id: 5, url: 'gallery/5.jpg', keywords: ['happy']},
+             {id: 6, url: 'gallery/6.jpg', keywords: ['happy']},
+             {id: 7, url: 'gallery/7.jpg', keywords: ['happy']},
+             {id: 8, url: 'gallery/8.jpg', keywords: ['happy']},
+             {id: 9, url: 'gallery/9.jpg', keywords: ['happy']},
+             {id: 10, url: 'gallery/10.jpg', keywords: ['happy']}
+            ];
+
+
+
+
+
+
+function setNewLine(newLine){
+    gMeme.selectedLineIdx =1;
+    gMeme.lines[1].txt = newLine;
+}
+
+function getImageById(imgId){
+    return gImgs.find(img => img.id === imgId);
+}
+
+function setMemeText(text){
+    gMeme.lines[0].txt = text;
+}
+
+function setFontFamily(font){
+    gMeme.lines[0].font = font;
+}
+
+function setTextAlign(textAlign){
+    gMeme.lines[0].align = textAlign;
+}
+
+function setTextColor(color){
+    gMeme.lines[0].align = textAlign;
+}
+
+function selectImg(imgId){
+    gMeme.selectedImgId = imgId;
+}          
+
+function getImages(){
+    return gImgs;
+}            
+
+function getMeme(){
+    return gMeme;
+} 
+
+function getLine(){
+    return gMeme.lines;
+} 
+
+
